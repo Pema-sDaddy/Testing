e488800a159bd35d2be212826f1f6cb450f42956
Todd Hughes <toddfhughes@yahoo.com>
2016-12-13 14:57:42 -0600

    Update map.js

--- docs/scripts/map.js
+++ docs/scripts/map.js
@@ -5,7 +5,7 @@
 //	'pk.eyJ1IjoicmFtb25hMjAyMCIsImEiOiI2ZjQzZTA4N2QxNjA5NzM2YjVhZTMwY2M1YmI2M2I2YSJ9.U1IwzOSQO-xjLU7NPxo-Dw';
 
 // ACTION ITEM: Insert the Mapbox key for your landing page map, refer blank for information on locating the map key. Also change the set view for your region of the world
-//var map = L.mapbox.map('map', "ramona2020.o3fkmamf").setView([42.36, -71.06],
+//var map = L.mapbox.map('map', "ramona2020.o3fkmamf").setView([54.915, -8.788],
 //	12);
 //var layer = L.mapbox.featureLayer().addTo(map)
 mapboxgl.accessToken = 'pk.eyJ1IjoidGZodWdoZXMiLCJhIjoiY2l0YW4yY3p1MDJpMjJubzYwaWIwbzVlOSJ9.1JIlibamCPwC32L28V42NA';
